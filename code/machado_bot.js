10dc309788f8e834da0082bd18a9736571b4e1ed
unknown <viniciusmachadoribeiro@hotmail.com>
2021-02-05 11:29:42 -0300

    calculadora basica implementada

--- code/machado_bot.js
+++ code/machado_bot.js
@@ -5,4 +5,35 @@ client.once('ready', () =>{
     console.log(`Logged in as ${client.user.tag}`)
 })
 
+client.on('message', message =>{
+    const arr = message.content.split(' ')
+    const [operation, ...rest] = arr
+
+    if (operation.toUpperCase() === 'SOMA'){
+        let result = 0
+        for (const i of rest){
+            result = result + Number(i)
+        }
+        message.channel.send(result)
+    } else if(operation.toUpperCase() === 'SUBTRACAO'){
+        let result = 2*Number(rest[0])
+        for (const i of rest){
+            result = result - Number(i)
+        }
+        message.channel.send(result)
+    } else if(operation.toUpperCase() === 'MULTIPLICACAO'){
+        let result = 1
+        for (const i of rest){
+            result = result * Number(i)
+        }
+        message.channel.send(result)
+    } else if(operation.toUpperCase() === 'DIVISAO'){
+        let result = Number(rest[0])**2
+        for (const i of rest){
+            result = result / Number(i)
+        }
+        message.channel.send(result)
+    }
+})
+
 client.login('token')
