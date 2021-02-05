a8c91aa5a9ff46bbc97b757d0f03e61cc16105d3
unknown <viniciusmachadoribeiro@hotmail.com>
2021-02-05 11:33:08 -0300

    funcao bhaskara implementada

--- code/machado_bot.js
+++ code/machado_bot.js
@@ -33,6 +33,19 @@ client.on('message', message =>{
             result = result / Number(i)
         }
         message.channel.send(result)
+    } else if(operation.toUpperCase() === 'BHASKARA'){
+        const a = Number(rest[0]), b = Number(rest[1]), c = Number(rest[2])
+        const delta = (b**2)-(4*a*c)
+        if (delta > 0){
+            const x1 = ((-b)+(Math.sqrt(delta)))/(2*a)
+            const x2 = ((-b)-(Math.sqrt(delta)))/(2*a)
+            message.channel.send(`S = {${x1}, ${x2}}`)
+        } else if(delta === 0){
+            const x = (-b)/(2*a)
+            message.channel.send(`S = {${x}`)
+        } else {
+            message.channel.send('Não possui raiz real.')
+        }
     }
 })
 
