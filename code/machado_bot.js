38f791e375b2b9813856b74f45d9886dfe27ee7c
unknown <viniciusmachadoribeiro@hotmail.com>
2021-02-08 11:05:19 -0300

    codigo atualizado e funcao pitagoras implementada

--- code/machado_bot.js
+++ code/machado_bot.js
@@ -7,35 +7,12 @@ client.once('ready', () =>{
 
 client.on('message', message =>{
     const arr = message.content.split(' ')
-    const [operation, ...rest] = arr
+    const mark = arr[0].toUpperCase()
 
-    if (operation.toUpperCase() === 'SOMA'){
-        let result = 0
-        for (const i of rest){
-            result = result + Number(i)
-        }
-        message.channel.send(result)
-    } else if(operation.toUpperCase() === 'SUBTRACAO'){
-        let result = 2*Number(rest[0])
-        for (const i of rest){
-            result = result - Number(i)
-        }
-        message.channel.send(result)
-    } else if(operation.toUpperCase() === 'MULTIPLICACAO'){
-        let result = 1
-        for (const i of rest){
-            result = result * Number(i)
-        }
-        message.channel.send(result)
-    } else if(operation.toUpperCase() === 'DIVISAO'){
-        let result = Number(rest[0])**2
-        for (const i of rest){
-            result = result / Number(i)
-        }
-        message.channel.send(result)
-    } else if(operation.toUpperCase() === 'BHASKARA'){
-        const a = Number(rest[0]), b = Number(rest[1]), c = Number(rest[2])
+    if(mark === 'BHASKARA'){
+        const a = Number(arr[1]), b = Number(arr[2]), c = Number(arr[3])
         const delta = (b**2)-(4*a*c)
+        
         if (delta > 0){
             const x1 = ((-b)+(Math.sqrt(delta)))/(2*a)
             const x2 = ((-b)-(Math.sqrt(delta)))/(2*a)
@@ -45,6 +22,27 @@ client.on('message', message =>{
             message.channel.send(`S = {${x}}`)
         } else {
             message.channel.send('Não possui raiz real.')
+        }
+    } else if(mark === 'PITAGORAS'){
+        const cat1 = Number(arr[1]), cat2 = Number(arr[2])
+
+        message.channel.send(`Hipotenusa = ${Math.sqrt((cat1**2)+(cat2**2))}`)
+    } else {
+        const [num1, operation, num2] = arr
+
+        switch (operation) {
+            case '+':
+                message.channel.send(Number(num1)+Number(num2))
+                break
+            case '-':
+                message.channel.send(Number(num1)-Number(num2))
+                break
+            case '*':
+                message.channel.send(Number(num1)*Number(num2))
+                break
+            case '/':
+                message.channel.send(Number(num1)/Number(num2))
+                break
         }
     }
 })
