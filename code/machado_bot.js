a1b2b956908573a57a87fa0113fdf24bdbd5d72b
unknown <viniciusmachadoribeiro@hotmail.com>
2021-02-08 14:32:27 -0300

    Comentarios adicionados ao codigo

--- code/machado_bot.js
+++ code/machado_bot.js
@@ -1,18 +1,22 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+//Evento que dispara ao logar o bot
 client.once('ready', () =>{
     console.log(`Logged in as ${client.user.tag}`)
 })
 
+//Evento que dispara ao receber mensagem em canais de texto
 client.on('message', message =>{
     const arr = message.content.split(' ')
     const mark = arr[0].toUpperCase()
 
+    //Checa a primeira palavra da mensagem inserida (mark)
     if(mark === 'BHASKARA'){
         const a = Number(arr[1]), b = Number(arr[2]), c = Number(arr[3])
         const delta = (b**2)-(4*a*c)
         
+        //Checa as condições de existência das raízes
         if (delta > 0){
             const x1 = ((-b)+(Math.sqrt(delta)))/(2*a)
             const x2 = ((-b)-(Math.sqrt(delta)))/(2*a)
@@ -27,9 +31,10 @@ client.on('message', message =>{
         const cat1 = Number(arr[1]), cat2 = Number(arr[2])
 
         message.channel.send(`Hipotenusa = ${Math.sqrt((cat1**2)+(cat2**2))}`)
-    } else {
+    } else if(Number(mark) !== NaN){
         const [num1, operation, num2] = arr
 
+        //Checa a operação que deve ser realizada
         switch (operation) {
             case '+':
                 message.channel.send(Number(num1)+Number(num2))
