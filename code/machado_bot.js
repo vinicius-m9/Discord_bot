deae994e4930ee58aa4319787d3623aabd4721e8
unknown <viniciusmachadoribeiro@hotmail.com>
2021-02-09 18:08:04 -0300

    comando help adicionado; verificacao removida das funcoes bhaskara e pitagoras

--- code/machado_bot.js
+++ code/machado_bot.js
@@ -21,34 +21,28 @@ client.on('message', message =>{
                 const a = Number(arr[2]), b = Number(arr[3]), c = Number(arr[4])
                 const delta = (b**2)-(4*a*c)
 
-                //Checa a validade da função
-                if(delta !== NaN){
-                    //Checa as condições de existência das raízes
-                    if (delta > 0){
-                        const x1 = ((-b)+(Math.sqrt(delta)))/(2*a)
-                        const x2 = ((-b)-(Math.sqrt(delta)))/(2*a)
-                        message.channel.send(`S = {${x1}, ${x2}}`)
-                    } else if(delta === 0){
-                        const x = (-b)/(2*a)
-                        message.channel.send(`S = {${x}}`)
-                    } else {
-                        message.channel.send('Não possui raiz real.')
-                    }
+                //Checa as condições de existência das raízes
+                if (delta > 0){
+                    const x1 = ((-b)+(Math.sqrt(delta)))/(2*a)
+                    const x2 = ((-b)-(Math.sqrt(delta)))/(2*a)
+                    message.channel.send(`S = {${x1}, ${x2}}`)
+                } else if(delta === 0){
+                    const x = (-b)/(2*a)
+                    message.channel.send(`S = {${x}}`)
                 } else {
-                    message.channel.send('Função inválida! Digite ". help" para saber mais.')
+                    message.channel.send('Não possui raiz real.')
                 }
                 
             break
             case 'PITAGORAS':
                 const cat1 = Number(arr[2]), cat2 = Number(arr[3])
-                
-                //Checa a validade da função
-                if(cat1 !== NaN & cat2 !== NaN){
-                    message.channel.send(`Hipotenusa = ${Math.sqrt((cat1**2)+(cat2**2))}`)
-                } else {
-                    message.channel.send('Função inválida! Digite ". help" para saber mais.')
-                }
+
+                message.channel.send(`Hipotenusa = ${Math.sqrt((cat1**2)+(cat2**2))}`)
     
+            break
+            case 'HELP':
+                message.channel.send('Funções válidas:\n. BHASKARA a b c\n. PITAGORAS cateto1 cateto2' +
+                '\nx + y\nx - y\nx * y\nx / y')
             break
             default:
                 message.channel.send('Função inválida! Digite ". help" para saber mais.')
