fcd7c14907d3fa92c094a3977ff0f08051b0de40
unknown <viniciusmachadoribeiro@hotmail.com>
2021-02-09 09:46:02 -0300

    arquivo machado_bot.js modificado

--- code/machado_bot.js
+++ code/machado_bot.js
@@ -9,29 +9,51 @@ client.once('ready', () =>{
 //Evento que dispara ao receber mensagem em canais de texto
 client.on('message', message =>{
     const arr = message.content.split(' ')
-    const mark = arr[0].toUpperCase()
+    
+    //Checa se o comando é uma função
+    if(message.content.startsWith('.') & arr[1] !== undefined){
+        const mark = arr[1].toUpperCase()
 
-    //Checa a primeira palavra da mensagem inserida (mark)
-    if(mark === 'BHASKARA'){
-        const a = Number(arr[1]), b = Number(arr[2]), c = Number(arr[3])
-        const delta = (b**2)-(4*a*c)
-        
-        //Checa as condições de existência das raízes
-        if (delta > 0){
-            const x1 = ((-b)+(Math.sqrt(delta)))/(2*a)
-            const x2 = ((-b)-(Math.sqrt(delta)))/(2*a)
-            message.channel.send(`S = {${x1}, ${x2}}`)
-        } else if(delta === 0){
-            const x = (-b)/(2*a)
-            message.channel.send(`S = {${x}}`)
-        } else {
-            message.channel.send('Não possui raiz real.')
+        //Checa a função que foi chamada
+        switch(mark){
+            case 'BHASKARA':
+                const a = Number(arr[2]), b = Number(arr[3]), c = Number(arr[4])
+                const delta = (b**2)-(4*a*c)
+
+                //Checa a validade da função
+                if(delta !== NaN){
+                    //Checa as condições de existência das raízes
+                    if (delta > 0){
+                        const x1 = ((-b)+(Math.sqrt(delta)))/(2*a)
+                        const x2 = ((-b)-(Math.sqrt(delta)))/(2*a)
+                        message.channel.send(`S = {${x1}, ${x2}}`)
+                    } else if(delta === 0){
+                        const x = (-b)/(2*a)
+                        message.channel.send(`S = {${x}}`)
+                    } else {
+                        message.channel.send('Não possui raiz real.')
+                    }
+                } else {
+                    message.channel.send('Função inválida! Digite ". help" para saber mais.')
+                }
+                
+            break
+            case 'PITAGORAS':
+                const cat1 = Number(arr[2]), cat2 = Number(arr[3])
+                
+                //Checa a validade da função
+                if(cat1 !== NaN & cat2 !== NaN){
+                    message.channel.send(`Hipotenusa = ${Math.sqrt((cat1**2)+(cat2**2))}`)
+                } else {
+                    message.channel.send('Função inválida! Digite ". help" para saber mais.')
+                }
+    
+            break
+            default:
+                message.channel.send('Função inválida! Digite ". help" para saber mais.')
         }
-    } else if(mark === 'PITAGORAS'){
-        const cat1 = Number(arr[1]), cat2 = Number(arr[2])
-
-        message.channel.send(`Hipotenusa = ${Math.sqrt((cat1**2)+(cat2**2))}`)
-    } else if(Number(mark) !== NaN){
+    //Checa se o comando é uma operação
+    } else if(Number(arr[0]) !== NaN){
         const [num1, operation, num2] = arr
 
         //Checa a operação que deve ser realizada
